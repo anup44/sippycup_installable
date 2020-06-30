87d5a4218d99bdb37705e85c4b9c836134a583d6
anup <anup.bhutada@sap.com>
2020-06-30 12:33:11 +0530

    made installable

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="sippycup",
     version="0.0",
-    author="Bill MacCartney"
+    author="Bill MacCartney",
     packages=["sippycup"],
     install_requires=[],
     url="https://github.com/wcmac/sippycup"
